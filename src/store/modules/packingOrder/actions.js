f21318137a6ef89613f31a89a4473c65b4ad74f6
junior <junior@edenfarm.id>
2023-02-07 15:29:13 +0700

    create packing & pack list

--- src/store/modules/packingOrder/actions.js
+++ src/store/modules/packingOrder/actions.js
@@ -5,6 +5,7 @@ const actions = {
         commit("setPreloadPackingOrderList", true)
         commit("setPackingOrderList", [])
         try {
+            let search = state.packing_order_list.filter.search
             let warehouse = !state.packing_order_list.filter.SelectWarehouse ? '' : state.packing_order_list.filter.SelectWarehouse
             let status = !state.packing_order_list.filter.statuses === 999 ? '' : state.packing_order_list.filter.statuses
             let delivery_date_from = ''
@@ -26,6 +27,7 @@ const actions = {
             }
             const response = await http.get('/site', {params: {
                 per_page:100,
+                search: search,
                 site_id: warehouse,
                 status: status,
                 delivery_date_from: delivery_date_from,
@@ -42,6 +44,7 @@ const actions = {
         }
     },
 
+    // Detail Packing Order
     fetchPackingOrderDetail: async ({ commit, dispatch}, payload) => {
         try {
             const response = await http.get("/packing_order/"+payload.id)
@@ -51,7 +54,59 @@ const actions = {
         } catch (error) {
             console.error(error)
         }
-    }
+    },
+
+    // Create Packing Order
+    createPackingOrder: async ({ commit, state, dispatch }, payload) => {
+        commit("setFormPackingOrderCreate", {
+            delivery_date:new Date(Date.now() + ( 3600 * 1000 * 7)).toISOString().substr(0, 10),
+            site_id:"",
+            note:"",
+        }); 
+    },
+
+    fetchPackList: async ({ state, commit }, payload) => {
+        commit("setPreloadPackList", true)
+        commit("setPackList", [])
+        try {
+            let search = state.pack_list.filter.search
+            let site = !state.pack_list.filter.SelectWarehouse ? '' : state.pack_list.filter.SelectWarehouse
+            let item = !state.pack_list.filter.SelectProduct ? '' : state.pack_list.filter.SelectProduct
+            let delivery_date_from = ''
+            let delivery_date_to = ''
+            if (state.pack_list.filter.delivery_date.value.length > 0) {
+                if (state.pack_list.filter.delivery_date.value.length == 1) {
+                delivery_date_from = state.pack_list.filter.delivery_date.value[0]
+                } else {
+                    let date = state.pack_list.filter.delivery_date.value[0]
+                    let date2 = state.pack_list.filter.delivery_date.value[1]
+                    if (date > date2) {
+                        delivery_date_from = date2
+                        delivery_date_to = date
+                    } else {
+                        delivery_date_from = date
+                        delivery_date_to = date2
+                    }
+                }
+            }
+            const response = await http.get('/site', {params: {
+                per_page:100,
+                search: search,
+                site_id: site,
+                item_id: item,
+                packing_date_from: delivery_date_from,
+                packing_date_to: delivery_date_to
+            }});
+            commit("setPackList", response.data.data)
+            if(response.data.data == null){
+                commit("setPackList", [])
+            }
+            commit("setPreloadPackList", false)
+        } catch (error) {
+            console.error(error)
+            commit("setPreloadPackList", false)
+        }
+    },
 }
 
 export default actions;
