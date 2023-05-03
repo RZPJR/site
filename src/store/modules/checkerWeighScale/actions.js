7ca67699c821a1c7af8d0ab7bee6ddf7ede29bc1
Tebe <zaki.baihaki@edenfarm.id>
2023-05-03 13:16:15 +0700

    Complete Weigh Scale

--- src/store/modules/checkerWeighScale/actions.js
+++ src/store/modules/checkerWeighScale/actions.js
@@ -34,15 +34,56 @@ const actions = {
         try {
             const response = await http.get('/checker_weight_scale/' + id);
             if(response.data.data){
-                commit('setLoading', false)
-                commit('setScanned', true)
                 commit('setFilterSetting', {...state.checker_weigh_scale.filter, search_prd: ''})
                 commit('setData', {...state.checker_weigh_scale.data, product: response.data.data})
-                // dispatch('fetchStableTime', {'type': 'percentage_kg_picking_tolerance'}) DONOT REMOVE, KEEP FOR REFERENCE
+                const res = await http.get('/configuration/v1/app', {
+                    params: {
+                        orderby: '-id'
+                    }
+                }, true)
+                if(res.data.data){
+                    commit('setLoading', false)
+                    commit('setScanned', true)
+                    let data = res.data.data
+                    for (let i = 0; i < data.length; i++) {
+                        let temp = data[i]
+                        if(temp.attribute === 'percentage_kg_picking_tolerance'){
+                            let temp_tolerance = temp.value
+                            let moq = 0.1
+                            let pct1 = (temp_tolerance / 100) * moq
+                            let pct2 = (temp_tolerance / 100) * state.checker_weigh_scale.data.product.order_qty
+                            commit('setData', {
+                                ...state.checker_weigh_scale.data,
+                                tolerance1: state.checker_weigh_scale.data.product.order_qty + pct1,
+                                tolerance2: state.checker_weigh_scale.data.product.order_qty + pct2
+                            })
+                        }
+                    }
+                }
             }
         } catch (error) {
             commit('setLoading', false)
             commit('setFilterSetting', {...state.checker_weigh_scale.filter, search_prd: ''})
+        }
+    },
+    updateProduct: async ({ commit, state, dispatch }, payload) => {
+        const {id, data} = payload
+        commit('setLoading', true)
+        try {
+            const response = await http.put('/checker_weight_scale/' + id, {
+                check_qty: parseFloat(data)
+            });
+            if(response.data.data){
+                commit('setLoading', false)
+                commit('setFilterSetting', {
+                    ...state.checker_weigh_scale.filter,
+                    alert: true,
+                    finished: true,
+                    caution: true,
+                })
+            }
+        } catch (error) {
+            commit('setLoading', false)
         }
     },
 
